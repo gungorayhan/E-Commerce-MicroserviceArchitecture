343acf51f16ad06987c03cc01ad448f63b0c6bed
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2024-06-10 09:47:11 +0300

    process of authenticate changed

--- gateway/index.js
+++ gateway/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const jwt =require("jsonwebtoken")
 const app=express();
 const PORT = 8000;
 const cors=require("cors")
@@ -8,19 +9,32 @@ console.log("gateway")
 app.use(express.json()) 
 
 
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
 
-console.log("auth")
+    jwt.verify(token, "secretKey", (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+
+
+
 app.use("/auth-service",proxy("http://localhost:7070"))
 console.log("order")
-app.use("/order-service",proxy("http://localhost:9090"))
+app.use("/order-service",authenticateToken,proxy("http://localhost:9090"))
 console.log("product")
-app.use("/product-service",proxy("http://localhost:8080"))
+app.use("/product-service",authenticateToken,proxy("http://localhost:8080"))
 app.use("/",(req,res)=>{
 
-    res.json({message:"deneme bağlantısı"})
+    res.json({message:"try connnection"})
     
 })
 
 app.listen(PORT,()=>{
-    console.log("Api Gateway 8000")
+    console.log(`Api Gateway running on port ${PORT}`)
 })
